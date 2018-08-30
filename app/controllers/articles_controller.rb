b154071989200ab852407e19601994eb5ecd909c
Jhon Jairo Mueses <jjmuesesq@unal.edu.co>
2018-08-30 04:58:45 +0000

    configuracion mostrando article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,21 @@
 class ArticlesController < ApplicationController
+    
+    
+    def index
+        @articles = Article.all
+    end
+      
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params[:article])
+ 
+        @article.save
+        redirect_to @article
     end
 end
